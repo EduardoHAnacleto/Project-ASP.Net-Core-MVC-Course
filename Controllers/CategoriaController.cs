b3389fa08536853d6bbe608009a8110c17fd3357
Eduardo H Anacleto <typhonedu@gmail.com>
2023-06-25 20:26:54 -0300

    update

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,12 +1,90 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebProjectCourse.Data;
+using WebProjectCourse.Models;
 
-namespace WebProjectCourse.Controllers
+namespace WebProjectCourse.Controllers;
+
+public class CategoriaController : Controller
 {
-    public class CategoriaController : Controller
+    private readonly AppDbContext _db;
+    public CategoriaController(AppDbContext db)
     {
-        public IActionResult Index()
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        List<Categoria> objListCategoria = _db.Categorias.ToList();
+        return View(objListCategoria);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Categoria obj)
+    {
+        if(ModelState.IsValid)
         {
-            return View();
+            _db.Categorias.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Categoria criada com sucesso.";
+            return RedirectToAction("Index");
         }
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Categoria? categoriaFromDb = _db.Categorias.Find(id);
+        if (categoriaFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoriaFromDb);
+    }
+    [HttpPost]
+    public IActionResult Edit(Categoria obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categorias.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Categoria alterada com sucesso.";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Categoria? categoriaFromDb = _db.Categorias.Find(id);
+        if (categoriaFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoriaFromDb);
+    }
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePOST(int? id)
+    {
+        Categoria obj = _db.Categorias.Find(id);
+        if (id == null)
+        {
+            return NotFound();
+        }
+        _db.Categorias.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Categoria apagada com sucesso.";
+        return RedirectToAction("Index");
     }
 }
--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -9,9 +9,10 @@ namespace WebProjectCourse.Models
         [Display(Name = "Código")]
         public int Id { get; set; }
         [Required]
-        [StringLength(30, MinimumLength = 3)]
+        [StringLength(30, MinimumLength = 3, ErrorMessage ="Nome deve ter entre 3 e 30 caracteres.")]
         public string Nome { get; set; }
         [Display(Name = "Ordem de display")]
+        [Range(1,100,ErrorMessage ="Ordem de display deve ser entre 1 e 100.")]
         public int OrdemDisplay { get; set; }
     }
 }
